7f8973cf8e65ab3ddbacbc0eeb572603f4df0f36
ipa-led <ludovic.delval@ipa.fraunhofer.de>
2018-01-31 12:02:03 +0100

    Create the introspection.py file

---
+++ src/smach_tutorial/solution/Introspection.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import smach
+import rospy
+import smach_ros
+
+import state_machine_tutorial
+from smach_tutorial.BasicStateMachine import *
+##-----------------------------------------------------------------------------------
+
+def main():
+    
+    SimpleSM = SetPrintStateMachine()
+    
+    introspection_server = smach_ros.IntrospectionServer('SM', SimpleSM, '/SM_root')
+    introspection_server.start()
+    
+    outcome = SimpleSM.execute()
+    rospy.loginfo("Result : " + outcome)
+    introspection_server.stop()
+    
+def main1():
+    
+    SimpleSM = FooBarStateMachine()
+    
+    introspection_server = smach_ros.IntrospectionServer('SM', SimpleSM, '/SM_root')
+    introspection_server.start()
+    
+    outcome = SimpleSM.execute()
+    rospy.loginfo("Result : " + outcome)
+    introspection_server.stop()
+    
+##-----------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    rospy.init_node('tutorial_node')
+    main()
+            
+
+        
